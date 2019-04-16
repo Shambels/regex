9479018014458e39f108d80fd0d27c3ac8f14b83
Shambels <Shambels@github.com>
2019-04-16 13:30:01 +0200

    TEST working on mobile ?

--- test.js
+++ test.js
@@ -1,11 +1,23 @@
 
 var txt = document.getElementById('txt');
 
-window.addEventListener('paste', () => {
-   let input = event.clipboardData.getData('text');
-   setTimeout(() => {
-      txt.value = input.replace(/-|_/g, ' ');
-      txt.select();
-      document.execCommand("copy");
-   }, 120);
-});
+
+if (typeof window.orientation !== 'undefined') {
+   window.addEventListener('change', () => {
+      let input = event.clipboardData.getData('text');
+      setTimeout(() => {
+         txt.value = input.replace(/-|_/g, ' ');
+         txt.select();
+         document.execCommand("copy");
+      }, 120);
+   });
+} else {
+   window.addEventListener('paste', () => {
+      let input = event.clipboardData.getData('text');
+      setTimeout(() => {
+         txt.value = input.replace(/-|_/g, ' ');
+         txt.select();
+         document.execCommand("copy");
+      }, 120);
+   });
+}
